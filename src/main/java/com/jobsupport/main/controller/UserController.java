345ca0a970712deb669f3eea9f35d056a3d64703
Sri-Charan-0516 <147786405+Sri-Charan-0516@users.noreply.github.com>
2024-02-13 17:34:03 +0530

    Charan (#8)
    * update
    
    * code
    
    * Updated Code
    
    * updated
    
    * Update
    
    * Updated Code
    
    * Code

--- src/main/java/com/jobsupport/main/controller/UserController.java
+++ src/main/java/com/jobsupport/main/controller/UserController.java
@@ -3,12 +3,16 @@ package com.jobsupport.main.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jobsupport.main.dto.ClientDto;
 import com.jobsupport.main.entity.User;
+import com.jobsupport.main.exceptions.InvalidUserIdException;
 import com.jobsupport.main.service.UserService;
 
 @RestController
@@ -24,6 +28,26 @@ public class UserController {
 		}
 		else {
 			return new ResponseEntity<User>(userService.addClient(clientDto),HttpStatus.CREATED);
+		}
+	}
+	
+	@PutMapping("/update/{email}")
+	public ResponseEntity<User> update(@PathVariable String email,@RequestBody User user) throws InvalidUserIdException{
+		if(email!=null) {
+			return new ResponseEntity<User>(userService.updateClient(email, user),HttpStatus.OK);
+		}
+		else {
+			throw new InvalidUserIdException("Email cant be null..!!");
+		}
+	}
+	
+	@GetMapping("/getDetails/{email}")
+	public ResponseEntity<User> getDetails(@PathVariable String email) throws InvalidUserIdException{
+		if(email.isEmpty()) {
+			throw new InvalidUserIdException("Email is empty..!!!");
+		}
+		else {
+			return new  ResponseEntity<User>(userService.getClientDetails(email),HttpStatus.ACCEPTED);
 		}
 	}
 }
